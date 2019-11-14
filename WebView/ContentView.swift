538000624095ffe8aa8cb8a5ea1ed3ebbf184111
Victorvs1967 <victorsmirnov@me.com>
2019-11-14 23:25:20 +0200

    Initial Commit

---
+++ WebView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebView
+//
+//  Created by Victor Smirnov on 14.11.2019.
+//  Copyright © 2019 Victor Smirnov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
